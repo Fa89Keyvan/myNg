8fa44b40df17dafcddbdbc039c3936c069cbfaa8
k.fathabadi <k.fathabadi@FATHABADI-PC>
2020-09-15 18:17:00 +0430

    2

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,22 +1,17 @@
 angular
-    .module("phoneList",[])
+    .module("phoneList", [])
     .component("phoneList", {
 
         templateUrl: "app/phone-list/phone-list.template.html",
-        controller: function PhoneController() {
-            this.phones = [
-                {
-                    name: 'Nexus S',
-                    snippet: 'Fast just got faster with Nexus S.'
-                },
-                {
-                    name: 'Motorola XOOM with Wi-Fi',
-                    snippet: 'The Next, Next Generation tablet.'
-                },
-                {
-                    name: 'MOTOROLA XOOM',
-                    snippet: 'The Next, Next Generation tablet.'
-                }
-            ]
-        }
+        controller: ['$http', function PhoneController($http) {
+
+            var self = this;
+            self.order = "name";
+
+            $http
+                .get("app/phone-list/phones.json")
+                .then(function (response) {
+                    self.phones = response.data;
+                });
+        }]
     });
